db7036f78a4600a8f635d386798c291fda9eeeed
deiznekcm <118221396+deiznekcm@users.noreply.github.com>
2023-01-06 15:01:11 -0500

    First push
    Added Deque interface

---
+++ DataStructures/src/basicdatastructures/Deque.java
@@ -0,0 +1,13 @@
+package basicdatastructures;
+
+public interface Deque<E> {
+	
+	public int size();
+	public boolean isEmpty();
+	public E getFirst() throws Exception;
+	public E getLast() throws Exception;
+	public void addFirst (E element);
+	public void addLast (E element);
+	public E removeFirst() throws Exception;
+	public E removeLast() throws Exception;
+}
